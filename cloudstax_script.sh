6cd9e56db60e176a41819e637fd5cd83d4c1ece7
Gabriel Oliveira Aguiar <gabriel.osilva@bandtec.com.br>
2022-05-25 17:03:33 -0300

    auto install script

---
+++ cloudstax_script.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+echo "Este é o script de instalação e configuração do Cloudfooding"
+echo "O script irá fazer diversas modificações na sua máquina"
+echo "Tem certeza que deseja continuar? S\n"
+read boolean
+
+if [ \"$boolean\" == \"s\" || \"$boolean\" == \"S\" ]
+then
+
+#Verificando e criando usuário
+echo "Fazendo verificação de usuário"
+getent passwd cloudstax
+if [ $? -eq 0 ]
+then echo \“Este usuário já está cadastrado\”
+else echo \"O usuário nao existe, criando usuario\"
+sudo adduser cloudstax
+sudo usermod -aG sudo cloudstax
+su cloudstax
+fi
+
+#Atualizando repositórios e pacotes
+echo "Atualizando repositórios e pacotes do sistema"
+sudo apt-get update && sudo apt-get upgrade -y
+
+#Verificando e instalando o Java
+echo "Verificando versão do Java"
+java --version
+if [ $? -eq 0 ]
+then echo \"O Java já está instalado\"
+else echo \"O Java está não instalado\"
+fi
+
+echo \"Gostaria de instalar o Java? S/n \"
+
+if 
+then
+sudo apt install default-jdk -y
+clear
+echo \"JAVA instalado na versao 11\"
+java --version
+sleep 5
+else echo \"você escolheu não instalar\"
+break
+fi
+clear
+
+echo \"Sera realizado agora, a instalacao cloudfooding, nosso sistema de monitoramento\"
+sleep 4
+clear
+
+echo \"Você deseja instalar e configrar o banco de dados local? S/n \"
+read boolean
+if [ \"$boolean\" == \"s\" || \"$boolean\" == \"S\" ]
+then
+sudo apt update && sudo apt upgrade -y
+clear
+sudo apt-get install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker pull mysql:5.7
+sudo docker build -t cloudstax_img:1.0 .
+sudo docker run -d -p 3306:3306 --name cloudstax cloudstax_img:1.0
+clear
+fi
+fi
