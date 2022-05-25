e9cec770d431bb37cfb221e0c0957a11e4fe9838
Gabriel Oliveira Aguiar <gabriel.osilva@bandtec.com.br>
2022-05-25 17:49:09 -0300

    script update

--- cloudstax_script.sh
+++ cloudstax_script.sh
@@ -21,7 +21,7 @@ echo "Atualizando repositórios e pacotes do sistema"
 sudo apt-get update && sudo apt-get upgrade -y
 
 echo "Instalando interface gráfica"
-sudo apt-get install lxde lxde-core xrdp tigervnc-standalone-server lightdm
+sudo apt-get install lxde lxde-core xrdp tigervnc-standalone-server lightdm -y
 
 #Verificando e instalando o Java
 echo "Verificando versão do Java"
